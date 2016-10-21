0a76c6a38049332d30510ba9ec86ff3eaaf35fa5
kryntsov <kryntsov@yandex.ru>
2016-10-21 11:50:37 +0700

    basic math operations in Calculate

--- Calculate.java
+++ Calculate.java
@@ -6,5 +6,17 @@ public class Calculate {
 		int second = Integer. valueOf (arg[1]);
 		int summ = first + second;
 		System.out.println ("Sum " + summ);
+		
+		int sub = first - second;
+		System.out.println ("Sub " + sub);
+		
+		long mult = first * second;
+		System.out.println ("Mult " + mult);
+		
+		int div = first / second;
+		System.out.println ("Div " + div);
+		
+		double exp = Math.pow (first, second);
+		System.out.println ("Exp " + exp);
 	}
 }
